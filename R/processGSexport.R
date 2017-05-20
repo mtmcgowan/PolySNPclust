d870643929c92e2cdd724631fc9fb420086a4538
Matthew McGowan <mmcgowan280@gmail.com>
2017-05-19 17:50:34 -0700

    Added Package description, Namespace, manual, and processGSexport function

---
+++ R/processGSexport.R
@@ -0,0 +1,38 @@
+#' Process Genome Studio Export
+#'
+#' Processes a Genome Studio data export table to only import useful information (theta and R values) and ignores the rest
+#'
+#' @param exportpath the path to the GS data export
+#'
+#' @return A list of two data.frames. The first is a table of theta values and the second is a table of intensity values
+#'
+#' @examples
+
+processGSexport <- function(exportpath = NULL) {
+  GSheader <-
+    as.character(fread(
+      exportpath,
+      header = F,
+      nrows = 1,
+      stringsAsFactors = F
+    ))
+  theta_ind <- grep('.Theta', GSheader)
+  r_ind <- grep('.R', GSheader)
+  theta_table <-
+    fread(
+      exportpath,
+      header = T,
+      nrows = -1,
+      stringsAsFactors = F,
+      select = c(2, theta_ind)
+    )
+  r_table <-
+    fread(
+      exportpath,
+      header = T,
+      nrows = -1,
+      stringsAsFactors = F,
+      select = c(2, r_ind)
+    )
+  return(list(theta_table, r_table))
+}
